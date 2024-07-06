61a81f2c63c33db26d1b48c57bee54f22ebede5f
Vishnu <144769004+vishnum1603@users.noreply.github.com>
2024-07-06 22:33:59 +0530

    SigninForm.jsx

--- Frontend/src/components/SigninForm.jsx
+++ Frontend/src/components/SigninForm.jsx
@@ -5,134 +5,128 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.min.css";
+import { Login } from "./services/Api";
 
-const defaultTheme = createTheme();
 
 export default function SignInSide() {
-  const handleSubmit = (event) => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = React.useState({
+    username: "",
+    password: "",
+  });
+  const [formErrors, setFormErrors] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+    setFormErrors({ ...formErrors, [name]: "" });
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    let isValid = true;
+    const newErrors = { ...formErrors };
+
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+      isValid = false;
+    }
+
+    setFormErrors(newErrors);
+
+    if (isValid) {
+      try {
+        console.log("Form is valid, submitting data:", formData);
+        setFormData({
+          username: "",
+          password: "",
+        });
+
+        const userData = await Login(formData);
+
+        if (userData && userData.token) {
+          console.log(userData.role) ;
+          console.log(userData.token) ;
+          if (userData.role === "ADMIN") {
+            toast.success("You have successfully signed in.", {
+              position: "top-center",
+              autoClose: 2000,
+              onClose: () => {
+                setTimeout(() => {
+                  navigate("/dashboard");
+                }, 1000);
+              },
+            });
+          } else {
+            toast.success("You have successfully signed in.", {
+              position: "top-center",
+              autoClose: 2000,
+              onClose: () => {
+                setTimeout(() => {
+                  navigate("/home");
+                }, 1000);
+              },
+            });
+          }
+        }
+      } catch (error) {
+        console.error("Error occurred while logging in:", error);
+        toast.error("Invalid Email or Password!!!", {
+          position: "top-center",
+          autoClose: 2000,
+        });
+      }
+    } else {
+      console.log("Form has errors");
+    }
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid
-        container
-        component="main"
-        sx={{
-          height: "89vh",
-          width: "93%",
-          margin: "6vh auto",
-          borderRadius: "10px",
-          overflow: "hidden",
-          boxShadow: "0px 0px 10px rgba(0, 0, 0.1, 0.3)",
-          position: "relative", 
-        }}
-      >
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            position: "absolute", // Position the background image absolutely
-            top: 0,
-            left: 0,
-            width: "50%",
-            height: "100%", 
-            backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[40]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="username"
+        label="User Name"
+        name="username"
+        autoComplete="username"
+        autoFocus
+        value={formData.username}
+        onChange={handleChange}
+        error={!!formErrors.username}
+        helperText={formErrors.username}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        value={formData.password}
+        onChange={handleChange}
+        error={!!formErrors.password}
+        helperText={formErrors.password}
+      />
+      <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Remember me"
+      />
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Sign In
+      </Button>
+    </Box>
   );
 }
